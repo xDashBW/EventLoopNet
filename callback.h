d42420afafb47c6cbe8171a55e8f5aa4fe69c594
xDashBW <xdashbw@163.com>
2023-02-25 00:00:45 +0000

    update

---
+++ callback.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "all.h"
+class Buffer;
+class TcpConnection;
+class Connector;
+class TcpClient;
+class ThreadPool;
+class Acceptor;
+class Channel;
+class TimerQueue;
+class Poll;
+class Epoll;
+class Thread;
+class Timer;
+class Select;
+
+typedef std::shared_ptr<Timer> TimerPtr;
+typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+typedef std::weak_ptr<TcpConnection> WeakTcpConnectionPtr;
+typedef std::shared_ptr<Connector> ConnectorPtr;
+typedef std::shared_ptr<TcpClient> TcpClientPtr;
+typedef std::shared_ptr<ThreadPool> ThreadPoolPtr;
+typedef std::unique_ptr<Acceptor> AcceptorPtr;
+typedef std::shared_ptr<Channel> ChannelPtr;
+typedef std::shared_ptr<TimerQueue> TimerQueuePtr;
+typedef std::shared_ptr<Poll> PollPtr;
+typedef std::shared_ptr<Epoll> EpollPtr;
+typedef std::shared_ptr<Select> SelectPtr;
+typedef std::shared_ptr<Thread> ThreadPtr;
+typedef std::function<void()> TimerCallback;
+typedef std::function<void(const TcpConnectionPtr &)> ConnectionCallback;
+typedef std::function<void(const TcpConnectionPtr &)> DisConnectionCallback;
+typedef std::function<void(const std::any &)> ConnectionErrorCallback;
+typedef std::function<void(const TcpConnectionPtr &)> CloseCallback;
+typedef std::function<void(const TcpConnectionPtr &)> WriteCompleteCallback;
+typedef std::function<void(const TcpConnectionPtr &, size_t)>
+    HighWaterMarkCallback;
+typedef std::function<void(const TcpConnectionPtr &, Buffer *)> MessageCallback;
---
+++ select.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifdef _WIN64
+#include "all.h"
+#include "log.h"
+#include "timerqueue.h"
+#include "util.h"
+
+class Channel;
+class EventLoop;
+class Select {
+ public:
+  typedef std::vector<Channel *> ChannelList;
+  typedef std::unordered_map<int32_t, Channel *> ChannelMap;
+  typedef std::vector<struct pollfd> EventList;
+
+  Select(EventLoop *loop);
+  ~Select();
+
+  void epollWait(ChannelList *activeChannels, int32_t msTime = 100);
+  void updateChannel(Channel *channel);
+  void removeChannel(Channel *channel);
+  bool hasChannel(Channel *channel);
+
+ private:
+  void fillActiveChannels(int32_t numEvents, ChannelList *activeChannels) const;
+
+  ChannelMap channels;
+  EventList events;
+  EventLoop *loop;
+
+  fd_set rfds;
+  fd_set wfds;
+  fd_set efds;
+};
+#endif
